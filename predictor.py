cd87c838bb6cd0efbeff439eec5874ca4624ffac
Abhishek Shelke <86852008+Redskull2525@users.noreply.github.com>
2026-03-17 00:05:33 +0530

    Refactor predictor.py for model and function updates

--- predictor.py
+++ predictor.py
@@ -1,15 +1,15 @@
+import yfinance as yf
 import numpy as np
 import pandas as pd
 import pickle
-import yfinance as yf
 
 from tensorflow.keras.models import load_model
 
-model = load_model("models/lstm_model.keras")
+model = load_model("models/transformer_model.keras")
 
 scaler = pickle.load(open("models/scaler.pkl","rb"))
 
-def fetch_live_data(ticker):
+def fetch_data(ticker):
 
     data = yf.download(
         ticker,
@@ -23,6 +23,7 @@ def fetch_live_data(ticker):
 def add_indicators(data):
 
     data["SMA20"] = data["Close"].rolling(20).mean()
+
     data["EMA20"] = data["Close"].ewm(span=20).mean()
 
     delta = data["Close"].diff()
@@ -37,30 +38,45 @@ def add_indicators(data):
 
     data["RSI"] = 100 - (100 / (1 + rs))
 
+    data["MA20"] = data["Close"].rolling(20).mean()
+
+    data["STD"] = data["Close"].rolling(20).std()
+
+    data["Upper"] = data["MA20"] + (2 * data["STD"])
+
+    data["Lower"] = data["MA20"] - (2 * data["STD"])
+
     data.dropna(inplace=True)
 
     return data
 
 
-def predict_next_minute(data):
+def predict(data):
 
     data = add_indicators(data)
 
-    features = data[["Close","Volume","SMA20","EMA20","RSI"]]
+    features = data[
+    [
+    "Close",
+    "Volume",
+    "SMA20",
+    "EMA20",
+    "RSI",
+    "Upper",
+    "Lower"
+    ]
+    ]
 
     scaled = scaler.transform(features)
 
-    last_window = scaled[-60:]
+    last = scaled[-60:]
 
     pred = model.predict(
-        last_window.reshape(1,60,last_window.shape[1])
+        last.reshape(1,60,last.shape[1])
     )
 
-    prediction = scaler.inverse_transform(
-        np.hstack(
-            [pred,
-             np.zeros((1,4))]
-        )
+    output = scaler.inverse_transform(
+        np.hstack([pred,np.zeros((1,6))])
     )
 
-    return float(prediction[0][0])
+    return float(output[0][0])
